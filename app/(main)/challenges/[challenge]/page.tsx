1ea520c11cbb5177f7d86948142e25c61228d993
Shawky Khalil <shawkykhalil@gmail.com>
2024-02-07 21:29:25 +0200

    ADD challenge page

---
+++ app/(main)/challenges/[challenge]/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import ChallengeCard from "@/components/Challenges/Card";
+import { ChallengeService } from "@/services/ChallengeService";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Challenges() {
+  const { data, isError, error, isSuccess } = useQuery({
+    queryKey: ["challenges"],
+    queryFn: ChallengeService.getAllChallenges,
+  });
+
+  if (isError) {
+    console.log(error);
+  }
+
+  return (
+    <main className="py-16">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 items-start">
+          {data?.map(({ id, ...rest }) => {
+            return <ChallengeCard key={id} challenge={rest} />;
+          })}
+        </div>
+      </div>
+    </main>
+  );
+}
